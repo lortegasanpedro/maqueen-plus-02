702093d7b486a8d3a51fad91d2c888de895d8fe8
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-03-15 23:58:00 +0100

    Comentarios

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-/**
- * Buscar LuzRight 
- * si  el nivel de luz es mayor de 70 para de buscar y retorna true
- * 
- */
+
+// Buscar LuzRight
+// si  el nivel de luz es mayor de 70 para de buscar y retorna true
 function buscarLuzRight (leftRight: boolean) {
     for (let index = 0; index <= 10; index++) {
         // serial.writeLine("Mov1: " + input.lightLevel())
@@ -21,12 +19,8 @@ function buscarLuzRight (leftRight: boolean) {
     }
     return false
 }
-
-/**
- * Buscar Luz
- * Dependiendo del paramtero direccion busca la luz en Left o en Right
- * 
- */
+// Buscar Luz
+// Dependiendo del paramtero direccion busca la luz en Left o en Right
 function buscarLuz (direccion: number) {
     if (direccion == 1) {
         if (!(buscarLuzRight(true))) {
@@ -46,12 +40,8 @@ function buscarLuz (direccion: number) {
         }
     }
 }
-
-/**
- * Buscar LuzLeft
- * si  el nivel de luz es mayor de 70 para de buscar y retorna true
- *
- */
+// Buscar LuzLeft
+// si  el nivel de luz es mayor de 70 para de buscar y retorna true
 function buscarLuzLeft (leftRight: boolean) {
     for (let index2 = 0; index2 <= 10; index2++) {
         // serial.writeLine("Mov1: " + input.lightLevel())
@@ -72,19 +62,25 @@ function buscarLuzLeft (leftRight: boolean) {
     maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
     return false
 }
-let VELOCIDAD = 0 // Velocidad para cuando el robot gira para buscar la luz
-let VELOCIDAD_FORWARD = 0 // Velocidad cuando el robot encuentra la luz y avanza hacia la misma
-let direccion: number // Si el valor es 1 busca primero en Right, si es 2 busca primero en Left
+ 
+ // Velocidad para cuando el robot gira para buscar la luz
+let VELOCIDAD = 0
+
+// let direccion Si el valor es 1 busca primero en Right, si es 2 busca primero en Left
+let direccion: number
+
 maqueenPlusV2.I2CInit()
+
 basic.forever(function () {
     // serial.writeLine("Luz: " + input.lightLevel())
     if (input.lightLevel() > 100) {
+        let VELOCIDAD_FORWARD = 0
         maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, VELOCIDAD_FORWARD)
         maqueenPlusV2.controlLED(maqueenPlusV2.MyEnumLed.AllLed, maqueenPlusV2.MyEnumSwitch.Open)
         maqueenPlusV2.ledBlank()
     } else if (input.lightLevel() > 80 && input.lightLevel() < 100) {
         direccion = randint(1, 2)
-        //serial.writeLine("Direccion: " + direccion)
+        // serial.writeLine("Direccion: " + direccion)
         buscarLuz(direccion)
     } else if (input.lightLevel() < 50) {
         maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
